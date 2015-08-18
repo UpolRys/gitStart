036c4346b44fb67494642c74e5688d4beebf447d
UpolRys <upol.ryskulova@gmail.com>
2015-08-18 12:01:51 +0300

    added unit test file

---
+++ testFile.py
@@ -0,0 +1,10 @@
+import unittest
+class sdfasdv(unittest.TestCase):
+	def test_bla(self):
+		print "testing started"
+		self.assertEqual(3+1,4)
+	def test_bla2(self):
+		self.assertEqual(3,2)
+
+if __name__ == "__main__":
+	unittest.main()
